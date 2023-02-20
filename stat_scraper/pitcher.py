84008b202df3ede7f033af7ff3073b33b76c7565
seungho Lee <seungho.lee@mytrum.com>
2023-02-20 16:37:56 +0900

    [fix] pitcher's stat scraper

--- stat_scraper/pitcher.py
+++ stat_scraper/pitcher.py
@@ -1,5 +1,5 @@
-
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 
 driver = webdriver.Chrome()
@@ -13,25 +13,25 @@ for year in yearScope:
               '&tm=&ty=0&qu=auto&po=0&as=&ae=&hi=&un=&pl=&da=2&o1=FIP&o2=WAR' \
               '&de=0&lr=0&tr=&cv=&ml=1&sn=30&si=&cn=' % (year, year, team)
         driver.get(url)
-        trs = driver.find_elements_by_css_selector('#mytable > tbody > tr')
+        trs = driver.find_elements(By.CSS_SELECTOR, '#mytable > tbody > tr')
         for index, item in enumerate(trs):
             if len(item.text) <= 5:
                 continue
-            if (item.find_elements_by_css_selector('th')):
+            if item.find_elements(By.CSS_SELECTOR, 'th'):
                 continue
-            name = item.find_element_by_css_selector('td:nth-child(2)').text  # 선수명
+            name = item.find_element(By.CSS_SELECTOR, 'td:nth-child(2)').text  # 선수명
             position = 'P'  # 포지션
-            G = item.find_element_by_css_selector('td:nth-child(5)').text  # 경기수
+            G = item.find_element(By.CSS_SELECTOR, 'td:nth-child(5)').text  # 경기수
             #Sp = item.find_element_by_css_selector('td:nth-child(25)').text  # 선발
-            Inning = item.find_element_by_css_selector('td:nth-child(6)').text  # 이닝
+            Inning = item.find_element(By.CSS_SELECTOR, 'td:nth-child(6)').text  # 이닝
             #hit = item.find_element_by_css_selector('td:nth-child(19)').text  # 피안타
             #BB = item.find_element_by_css_selector('td:nth-child(10)').text  # 피볼넷
             #Dead_Ball = item.find_element_by_css_selector('td:nth-child(11)').text  # 사구
             #SO = item.find_element_by_css_selector('td:nth-child(12)').text  # 삼진
             #Wild_pitch = item.find_element_by_css_selector('td:nth-child(17)').text  # 폭투
             #BLK = item.find_element_by_css_selector('td:nth-child(15)').text  # 보크
-            avg = item.find_element_by_css_selector('td:nth-child(19)').text  # 피안타율
-            obp = item.find_element_by_css_selector('td:nth-child(20)').text  # 피출루율
+            avg = item.find_element(By.CSS_SELECTOR, 'td:nth-child(19)').text  # 피안타율
+            obp = item.find_element(By.CSS_SELECTOR, 'td:nth-child(20)').text  # 피출루율
 
             doc = {
                 'name': name, 'position': position, 'G': G, 'Inning': Inning,
