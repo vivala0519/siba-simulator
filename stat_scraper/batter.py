44664ce0048c9903136cecd22b4cc54e13c2e9ff
vivala5019 <vivala0519@naver.com>
2023-02-21 14:32:15 +0900

    [fix] batter.py for json file

--- stat_scraper/batter.py
+++ stat_scraper/batter.py
@@ -1,17 +1,21 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
+import json
 
-# client = MongoClient('localhost', 27017)
-# db = client.project
+file_path = './batter.json'
+
+data = {}
+
 driver = webdriver.Chrome()
 
 # 연도범위 동안 팀리스트 스크래핑 후 DB에 저장
-yearScope = range(1982, 1983)
-# team_List = ['SK', 'SSG', '삼성', 'NC', '한화', 'LG', '롯데', 'KIA', 'kt', '히어로즈', '두산', '해태', '현대', '청보',
-#              '삼미', 'MBC', 'OB', '태평양', '빙그레', '쌍방울']
-team_List = ['삼성']
+yearScope = range(1982, 2023)
+team_List = ['SK', 'SSG', '삼성', 'NC', '한화', 'LG', '롯데', 'KIA', 'kt', '히어로즈', '두산', '해태', '현대', '청보',
+             '삼미', 'MBC', 'OB', '태평양', '빙그레', '쌍방울']
+
 for year in yearScope:
+    data[year] = []
     for team in team_List:
         url = 'http://www.statiz.co.kr/stat.php?opt=0&sopt=0&re=0&ys=%d&ye=%d&se=0&te=%s' \
               '&tm=&ty=0&qu=auto&po=0&as=&ae=&hi=&un=&pl=&da=1&o1=WAR_ALL_ADJ&o2=TPA&de=1' \
@@ -29,6 +33,7 @@ for year in yearScope:
                     continue
             except NoSuchElementException:
                 print('position not found')
+                continue
             name = item.find_element(By.CSS_SELECTOR, 'td:nth-child(2)').text  # 선수명
             position = item.find_element(By.CSS_SELECTOR, 'td:nth-child(3) > span > span:nth-child(3)').text # 포지션
             avg = item.find_element(By.CSS_SELECTOR, 'td:nth-child(24)').text  # 타율
@@ -48,7 +53,15 @@ for year in yearScope:
                 'double_hit': double_hit, 'triple_hit': triple_hit, 'home_run': home_run, 'BB': BB, 'SB': SB,
                 'Dead_Ball': Dead_Ball, 'one_hit': one_hit, 'year': year, 'team': team
             }
-            # db.kbo_batter_stat.insert_one(doc)
+            data[year].append({
+                'name': name, 'team': team, 'position': position, 'avg': avg, 'obp': obp, 'slg': slg, 'total_hit': total_hit,
+                'double_hit': double_hit, 'triple_hit': triple_hit, 'home_run': home_run, 'BB': BB, 'SB': SB,
+                'Dead_Ball': Dead_Ball, 'one_hit': one_hit,
+            })
+            
             print(year, team, name, '저장 완료')
+
+            with open(file_path, 'w', encoding='utf-8') as file:
+                json.dump(data, file, indent=4, ensure_ascii=False)
 
 driver.quit()
